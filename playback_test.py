a4d92c2cfb0b7fba4baa2564d0dcd8b2b3d57883
Henry <hcp11502@gmail.com>
2026-01-21 22:32:04 -0500

    Updated 'playback_test.py' to reflect changes to Library and Player classes

--- playback_test.py
+++ playback_test.py
@@ -8,13 +8,28 @@ if __name__ == "__main__":
     test_library_path = os.path.abspath(test_library_path)
 
     test_library = Library()
-    test_library.initialize_library(test_library_path)
+    test_library.clear_database()
+    test_library.initialize_database(test_library_path)
     test_queue = test_library.get_all_songs()
     print('Total songs in library: {}'.format(len(test_queue)))
 
     player = Player(test_queue)
     player.play()
-    #print('Now Playing: {} by {}'.format(player.get_current_song().get_title(), player.get_current_song().get_artist()))
-    time.sleep(20)
+
+    while True:
+        if player.get_current_song():
+            break
+
+    print('Now Playing: {} by {}'.format(player.get_current_song().get_title(), player.get_current_song().get_artist()))
+    
+    time.sleep(5)
+    player.pause()
+    time.sleep(5)
+    player.play()
+    time.sleep(5)
     player.stop()
+    time.sleep(5)
+    player.play()
+    time.sleep(5)
+
     print('Test complete.')
